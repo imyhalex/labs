40d163a75668709dd183a8f9d9f3b370730aa995
imyhalex <ys4689@nyu.edu>
2024-11-02 17:57:11 -0400

    milestone2

--- nyuenc/nyuenc.c
+++ nyuenc/nyuenc.c
@@ -8,187 +8,345 @@
 #include <unistd.h>
 #include <sys/mman.h>
 #include <pthread.h>
-#include <unistd.h>
 #include <string.h>
+#include <getopt.h>
 
 #define BUFFER_SIZE 16384 // define a 16KB buffer
-#define CHUNCK_SIZE 4096
+#define CHUNK_SIZE 4096
+#define MAX_FILES 100
+#define MAX_CHUNKS 250000
 
-/** the info of each thread should contain */
-typedef struct {
-    int is_first_file;
-    unsigned char curr_c;
-    unsigned char count;
-    unsigned char *buffer;
-    size_t buffer_index;
-    size_t buffer_size;
-} EncoderState;
+/**
+ * All milestone 2 borrowed ideas from:
+ * @cite: https://github.com/mathewpan2/Multithreaded-RLE/blob/main/encode.c
+ * useed 90% of structs and global variables
+ * ideas of 'void parallel_rle(char **files, int num_files_input, int num_threads);' from multi-thread part in 'int main();'
+ * ideas of 'void *fetch_task();' are from 'void* worker_encode();'
+ */
+typedef struct task_node {
+    int index;
+    size_t start;
+    size_t end;
+    unsigned char *data; // each node references entire data
+    struct task_node *next;
+} Node;
 
-/** a struct for a task */
 typedef struct {
-    int task_id;
-    char *data;
-    size_t size;
-} Task;
-
-/** a struct for task queue - blue part of the image */
-typedef struct {
-    Task *tasks;
-    int capacity;               // current capacity of the task queue
-    int size;                   // number of tasks currently in the queue       
-    int front;                  // index of the front of the queue
-    int rear;                   // index of the rear of the queue
-    pthread_mutex_t mutex;      // mutex to protect access to the queue
-    pthread_cond_t not_empty;   // condition variable to signal when the queue is not empty
+    Node *front;
+    Node *end;
 } TaskQueue;
 
-/** global variable for write mutex */
-pthread_mutex_t write_mutex = PTHREAD_MUTEX_INITIALIZER;
+/** some global values here */
+TaskQueue queue;
+pthread_mutex_t queue_mutex = PTHREAD_MUTEX_INITIALIZER;
+pthread_cond_t queue_cond = PTHREAD_COND_INITIALIZER;
 
-void init_task_queue(TaskQueue *queue)
+unsigned char **results; 
+int chunk_size_arr[MAX_CHUNKS];
+int completed_arr[MAX_CHUNKS] = {0}; // flagger array to keep track if each task is completed
+pthread_mutex_t results_mutex = PTHREAD_MUTEX_INITIALIZER;
+pthread_cond_t results_cond = PTHREAD_COND_INITIALIZER;
+pthread_mutex_t result_mutex = PTHREAD_MUTEX_INITIALIZER;
+pthread_cond_t result_cond = PTHREAD_COND_INITIALIZER;
+
+// file handling
+struct stat sb_files[MAX_FILES];
+unsigned char *addr_files[MAX_FILES];
+int num_files = 0;
+
+// control variables
+int all_tasks_created = 0;
+
+// sequential variables
+int is_first_file = 1;
+unsigned char curr_c = 0;
+unsigned char buffer[BUFFER_SIZE];
+size_t buffer_index = 0;
+// 1-byte unsigned integer in binary format
+unsigned char count = 0;
+
+void init_queue(TaskQueue *queue)
 {
-    queue->capacity = 10; // Initial capacity
-    queue->tasks = malloc(queue->capacity * sizeof(Task));
-    queue->size = 0;
-    queue->front = 0;
-    queue->rear = 0;
-    pthread_mutex_init(&queue->mutex, NULL);
-    pthread_cond_init(&queue->not_empty, NULL);
+    queue->front = queue->end = NULL;
 }
 
-void enqueue_task(TaskQueue *queue, Task task)
+void task_enqueue(TaskQueue *queue, Node *node)
 {
-    pthread_mutex_lock(&queue->mutex);
-    if (queue->size == queue->capacity)
+    node->next = NULL;
+    if (queue->end == NULL)
+        queue->front = queue->end = node;
+    else
     {
-        queue->capacity *=2;
-        queue->tasks = realloc(queue->tasks, queue->capacity * sizeof(Task));
-        if (!queue->tasks) 
+        queue->end->next = node;
+        queue->end = node; // update tne end pointer point to the new end
+    }
+}
+
+Node *task_dequeue(TaskQueue *queue)
+{
+    if (queue->front == NULL)
+        return NULL;
+    Node *node = queue->front;
+    queue->front = queue->front->next;
+    if (queue->front == NULL)
+        queue->end = NULL;
+    node->next = NULL;
+    return node;
+}
+
+void *fetch_task()
+{
+    Node *task;
+    for (;;)
+    {
+        pthread_mutex_lock(&queue_mutex);
+        while (queue.front == NULL && !all_tasks_created)
+            pthread_cond_wait(&queue_cond, &queue_mutex);
+        if (queue.front == NULL && all_tasks_created)
         {
-            perror("malloc");
+            pthread_mutex_unlock(&queue_mutex);
+            break;
+        }
+        task = task_dequeue(&queue);
+        pthread_mutex_unlock(&queue_mutex);
+        if (task == NULL) continue;
+        // do rle
+        unsigned char *ptr = task->data + task->start;
+        size_t size = task->end - task->start;
+        unsigned char local_count = 0;
+        unsigned char local_char = 0;
+        unsigned char *local_buffer = (unsigned char *) malloc((size * 2) + 1);
+        int local_index = 0;
+
+        size_t i;
+        for (i = 0; i < size; i++)
+        {   
+            if (local_count == 0)
+            {
+                local_char = ptr[i];
+                local_count = 1;
+            }
+            else if (ptr[i] == local_char && local_count < 255)
+                local_count++;
+            else
+            {
+                local_buffer[local_index++] = local_char;
+                local_buffer[local_index++] = local_count;
+                local_char = ptr[i];
+                local_count = 1; 
+            }
+        }
+        if (local_count > 0)
+        {
+            local_buffer[local_index++] = local_char;
+            local_buffer[local_index++] = local_count;
+        }
+        // store the result
+        pthread_mutex_lock(&results_mutex);
+        results[task->index] = local_buffer;
+        chunk_size_arr[task->index] = local_index;
+        completed_arr[task->index] = 1;
+        pthread_cond_signal(&results_cond);
+        pthread_mutex_unlock(&results_mutex);
+        free(task);
+    }
+    return NULL;
+}
+
+void parallel_rle(char **files, int num_files_input, int num_threads)
+{
+    num_files = num_files_input;
+    results = (unsigned char **)malloc(MAX_CHUNKS * sizeof(unsigned char *));
+    int i;
+    for (i = 0; i < num_files; i++)
+    {
+        int fd = open(files[i], O_RDONLY);
+        if (fd == -1) 
+        {
+            perror("open");
             exit(1);
         }
+
+        if (fstat(fd, &sb_files[i]) == -1)
+        {
+            perror("fstat");
+            close(fd);
+            exit(1);
+        }
+
+        addr_files[i] = mmap(NULL, sb_files[i].st_size, PROT_READ, MAP_PRIVATE, fd, 0);
+        if (addr_files[i] == MAP_FAILED)
+        {
+            perror("mmap");
+            close(fd);
+            exit(1);
+        }
+        close(fd); 
     }
-    queue->tasks[queue->rear] = task;
-    queue->rear = (queue->rear + 1) % queue->capacity;
-    queue->size++;
-    pthread_cond_signal(&queue->not_empty);
-    pthread_mutex_unlock(&queue->mutex);
-}
 
-int dequeue_task(TaskQueue *queue, Task *task)
-{
-    pthread_mutex_lock(&queue->mutex);
-    while (queue->size == 0) 
-        pthread_cond_wait(&queue->not_empty, &queue->mutex);
-    *task = queue->tasks[queue->front];
-    queue->front = (queue->front + 1) % queue->capacity;
-    queue->size--;
-    pthread_mutex_unlock(&queue->mutex);
-    return 1;
-}
-
-void destory_queue(TaskQueue *queue)
-{
-    free(queue->tasks);
-    pthread_mutex_destroy(&queue->mutex);
-    pthread_cond_destroy(&queue->not_empty);
-}
-
-void flush_buffer(EncoderState *state)
-{
-    // only flush it when having data
-    if (state->buffer_index > 0)
+    init_queue(&queue);
+    pthread_t *threads = (pthread_t *)malloc(num_threads * sizeof(pthread_t));
+    for (i = 0; i < num_threads; i++)
+        pthread_create(&threads[i], NULL, fetch_task, NULL);
+    
+    // enqueue tasks
+    int chunk_idx = 0;
+    int total_chunks = 0;
+    for (i = 0; i < num_files; i++)
     {
-        pthread_mutex_lock(&write_mutex);
-        write(STDOUT_FILENO, state->buffer, state->buffer_index);
-        pthread_mutex_unlock(&write_mutex);
-        // reset buffer index
-        state->buffer_index = 0;
+        size_t file_size = sb_files[i].st_size;
+        size_t chunks = file_size / CHUNK_SIZE;
+        size_t remainder = file_size % CHUNK_SIZE;
+        if (remainder != 0)
+            chunks++;
+        
+        size_t j;
+        for (j = 0; j < chunks; j++)
+        {
+            size_t start = j * CHUNK_SIZE;
+            size_t end = (j == chunks - 1) ? file_size : (j + 1) * CHUNK_SIZE;
+            Node *task = (Node *)malloc(sizeof(Node));
+            task->index = chunk_idx;
+            task->start = start;
+            task->end = end;
+            task->data = addr_files[i];
+            task->next = NULL;
+
+            pthread_mutex_lock(&queue_mutex);
+            task_enqueue(&queue, task);
+            pthread_cond_signal(&queue_cond);
+            pthread_mutex_unlock(&queue_mutex);
+
+            chunk_idx++;
+            total_chunks++;
+        }
+    }
+
+    // all tasks have been created
+    pthread_mutex_lock(&queue_mutex);
+    all_tasks_created = 1;
+    pthread_cond_broadcast(&queue_cond);
+    pthread_mutex_unlock(&queue_mutex);
+
+    // wait all threads
+    for (i = 0; i < num_threads; i++)
+        pthread_join(threads[i], NULL);
+
+    // merge boundary - sequential
+    unsigned char last_char = 0;
+    unsigned char last_count = 0;
+    int has_last_run = 0; // flag to indicate if there's a pending run to merge
+    int j = 0;
+    for (j = 0; j < total_chunks; j++)
+    {
+        // skip if empty
+        if (chunk_size_arr[j] < 2)
+            continue;
+        int k;
+        for (k = 0; k < chunk_size_arr[j]; k+=2)
+        {
+            unsigned char current_char = results[j][k];
+            unsigned char current_count = results[j][k + 1];
+            if (has_last_run && current_char == last_char)
+            {
+                unsigned int combined_count = (unsigned int)last_count + (unsigned int)current_count;
+                if (combined_count <= 255)
+                    last_count = (unsigned char)combined_count;
+                else
+                {
+                    // write out the maximum allowed count
+                    unsigned char overflow = (unsigned char)(combined_count - 255);
+                    fwrite(&last_char, 1, 1, stdout);
+                    fwrite(&last_count, 1, 1, stdout);
+                    last_count = overflow;
+                }
+            }
+            else 
+            {
+                if (has_last_run)
+                {
+                    fwrite(&last_char, 1, 1, stdout);
+                    fwrite(&last_count, 1, 1, stdout);
+                }
+                last_char = current_char;
+                last_count = current_count;
+                has_last_run = 1;
+            }
+        }
+    }
+
+    if (has_last_run)
+    {
+        fwrite(&last_char, 1, 1, stdout);
+        fwrite(&last_count, 1, 1, stdout);
+    }
+
+    // clean up
+    free(queue.front);
+    free(queue.end);
+    pthread_mutex_destroy(&queue_mutex); 
+    pthread_cond_destroy(&queue_cond);
+    pthread_cond_destroy(&results_cond);
+    pthread_mutex_destroy(&results_mutex);
+    free(threads);
+    for (i = 0; i < num_files; i++)
+        munmap(addr_files[i], sb_files[i].st_size);
+    free(results);
+}
+
+/** this part for sequential rle */
+// helper function for sequential rle
+void flush_buffer()
+{
+    if (buffer_index > 0)
+    {
+        write(STDOUT_FILENO, buffer, buffer_index);
+        buffer_index = 0;
     }
 }
 
-void encode_to_rle(char *data, size_t size, EncoderState *state)
+void encode_to_rle(char *data, size_t size)
 {
     unsigned char *ptr = (unsigned char *) data;
-    // find the end of the pointer
     unsigned char *end = ptr + size;
 
     while (ptr < end)
     {
-        if (state->is_first_file)
-        {
-            state->count = 1;
-            state->curr_c = *ptr;
-            state->is_first_file = 0;
+        // if the first character
+        if (is_first_file)
+        {   
+            count = 1;
+            curr_c = *ptr;
+            is_first_file = 0;
         }
-        else if (*ptr == state->curr_c && state->count < 255)
-            state->count++;
+        // if character appear within 255 times in a row and pointer is equals to current character
+        else if (*ptr == curr_c && count < 255)
+            count++;
+        // write the character and count to the buffer
         else
         {
-            // Append to buffer
-            state->buffer[state->buffer_index++] = state->curr_c;
-            state->buffer[state->buffer_index++] = state->count;
+            buffer[buffer_index++] = curr_c;
+            buffer[buffer_index++] = count;
 
-            // Flush buffer if necessary
-            if (state->buffer_index >= state->buffer_size - 2)
-                flush_buffer(state);
+            // if buffer is full, flush the output
+            if (buffer_index >= BUFFER_SIZE - 2)
+                flush_buffer();
 
-            // Reset curr_c and count
-            state->curr_c = *ptr;
-            state->count = 1;
+            // reset curr_c and count
+            curr_c = *ptr;
+            count = 1;
         }
         ptr++;
     }
 }
 
-// worker thread function
-void *worker_function(void * arg)
-{
-    TaskQueue *task_queue = (TaskQueue *) arg;
-
-    // initialize encoder state
-    EncoderState state;
-    state.is_first_file = 1;
-    state.curr_c = 0;
-    state.count = 0;
-    state.buffer_size = BUFFER_SIZE;
-    state.buffer = malloc(BUFFER_SIZE);
-    state.buffer_index = 0;
-    if (!state.buffer)
-    {
-        perror("malloc");
-        exit(1);
-    }
-
-    while(1)
-    {
-        Task task;
-        dequeue_task(task_queue, &task);
-        if (task.task_id == -1) 
-            break;
-        encode_to_rle(task.data, task.size, &state);
-
-        if (state.count > 0)
-        {
-            state.buffer[state.buffer_index++] = state.curr_c;
-            state.buffer[state.buffer_index++] = state.count;
-            state.count = 0;
-        }
-        flush_buffer(&state);
-    }
-
-    free(state.buffer);
-    return NULL;
-}
-
-int map_file(const char *filename, char **data, size_t *size)
+void write_rle(const char *filename)
 {
     int fd = open(filename, O_RDONLY);
     if (fd == -1)
     {
         perror("open");
-        return -1;
+        return;
     }
 
     // Get file size
@@ -197,39 +355,44 @@ int map_file(const char *filename, char **data, size_t *size)
     {
         perror("fstat");
         close(fd);
-        return -1;
+        return;
     }
 
     // Map file into memory
-    *data = mmap(NULL, sb.st_size, PROT_READ, MAP_PRIVATE, fd, 0);
-    if (*data == MAP_FAILED)
+    char *addr = mmap(NULL, sb.st_size, PROT_READ, MAP_PRIVATE, fd, 0);
+    if (addr == MAP_FAILED)
     {
         perror("mmap");
         close(fd);
-        return -1;
+        return;
     }
 
-    *size = sb.st_size;
+    // do rle encoding
+    encode_to_rle(addr, sb.st_size);
+
+    // remove mapping
+    munmap(addr, sb.st_size);
     close(fd);
-    return 0;
 }
 
-int main(int argc, char **argv)
+/**
+ * @cite: https://www.man7.org/linux/man-pages/man3/getopt.3.html
+ */
+int main (int argc, char **argv)
 {
-    /** @cite: https://www.man7.org/linux/man-pages/man3/getopt.3.html */
-    // default value for thread is 1 (sequential)
-    int num_threads = 1;
     int opt;
-
+    int num_threads = 0;
+    int num_files_input = 0;
+    char **file_list = NULL;
     while ((opt = getopt(argc, argv, "j:")) != -1)
     {
-        switch(opt)
+        switch (opt)
         {
             case 'j':
                 num_threads = atoi(optarg);
-                if (num_threads <= 0) 
+                if (num_threads <= 0)
                 {
-                    fprintf(stderr, "Invalid number of threads.\n");
+                    fprintf(stderr, "Invalid number of threads: %s\n", optarg);
                     exit(1);
                 }
                 break;
@@ -239,63 +402,33 @@ int main(int argc, char **argv)
         }
     }
 
-    if (optind >= argc)
+    num_files_input = argc - optind;
+    file_list = &argv[optind];
+
+    if (num_files_input < 1)
     {
-        fprintf(stderr, "Expected argument after options\n");
+        fprintf(stderr, "No input files specified.\n");
         exit(1);
     }
 
-    // initialize a task queue (blue section)
-    TaskQueue queue;
-    init_task_queue(&queue);
-
-    // worker thread pool
-    pthread_t *threads = malloc(num_threads * sizeof(pthread_t));
-    int i;
-    for (i = 0; i < num_threads; i++)
-        pthread_create(&threads[i], NULL, worker_function, &queue);
-    
-    int task_id = 0;
-    for (i = optind; i < argc; i++)
+    if (num_threads > 0)
+        parallel_rle(file_list, num_files_input, num_threads);
+    else
     {
-        char *data;
-        size_t file_size;
-        if (map_file(argv[i], &data, &file_size) != 0)
-            continue;
+        int i;
+        for (i = 0; i < num_files_input; i++)
+            write_rle(file_list[i]);
         
-        // enqueue chunks in 4KB (4096 Bytes)
-        size_t offset = 0;
-        while (offset < file_size)
+        // write last char and count after writing all files
+        if (!is_first_file && count > 0)
         {
-            size_t chunck_size = (file_size - offset >= 4096) ? 4096 : (file_size - offset);
-            // create a task
-            Task task;
-            task.task_id = task_id++;
-            task.data = data + offset;
-            task.size = chunck_size;
-            enqueue_task(&queue, task);
-            // increment the offset for next chunk condition
-            offset += chunck_size;
+            buffer[buffer_index++] = curr_c;
+            buffer[buffer_index++] = count;
         }
-        // unmap the file
-        // munmap(data, file_size);
-    }
 
-    // a task for termination - flag for terminate the task queue
-    for (i = 0; i < num_threads; i++)
-    {
-        Task term_task;
-        term_task.task_id = -1;
-        term_task.data = NULL;
-        term_task.size = 0;
-        enqueue_task(&queue, term_task);
+        // flush the remaining data
+        flush_buffer();
+        exit(0);
     }
-
-    // wait for woker pools to finish
-    for (i = 0; i < num_threads; i++)
-        pthread_join(threads[i], NULL);
-    free(threads);
-    destory_queue(&queue);
-    pthread_mutex_destroy(&write_mutex);
-    exit(0);
+    return 0;
 }
